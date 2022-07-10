608c3a6676146e3deb8a6404b2368a7dded6b0ea
Angel De Luna <78061993+EllipticSea9@users.noreply.github.com>
2022-07-09 19:24:17 -0500

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,20 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.0/firebase-app.js";
+  import {} from "https://www.gstatic.com/firebasejs/8.10.1/firebase-auth.js"
+  import {} from "https://www.gstatic.com/firebasejs/8.10.1/firebase-firestore.js"
+
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCrT--4lonz7-AJnJRwCi7vL8ZuC7RUWr0",
+    authDomain: "agenda-1a04f.firebaseapp.com",
+    projectId: "agenda-1a04f",
+    storageBucket: "agenda-1a04f.appspot.com",
+    messagingSenderId: "379226191683",
+    appId: "1:379226191683:web:473e46787fbe18f55ca1f6"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
